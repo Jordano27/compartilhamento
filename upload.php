ce5409caed245b65fc98af8a0ca9ffe0226785ea
Wouters299 <126169358+Wouters299@users.noreply.github.com>
2023-05-12 11:14:51 -0300

    search

--- upload.php
+++ upload.php
@@ -1,51 +1,64 @@
-<?php 
-  require('verifica_login.php');
-  require('twig_carregar.php');
-  require('pdo.inc.php');
-  require('func/sanitize_filename.php');
-    $localhost = "localhost"; #localhost
-    $dbusername = "root"; #username of phpmyadmin
-    $dbpassword = "";  #password of phpmyadmin
-    $dbname = "usuario";  #database name
 
-    #connection string
-    $pdo = mysqli_connect($localhost,$dbusername,$dbpassword,$dbname);
+<?php
+require('verifica_login.php');
+require('twig_carregar.php');
+require('pdo.inc.php');
+require('func/sanitize_filename.php');
 
-    if (mysqli_connect_errno()) {
-        echo "Failed to connect to MySQL: " . mysqli_connect_error();
+$localhost = "localhost"; #localhost
+$dbusername = "root"; #username of phpmyadmin
+$dbpassword = "";  #password of phpmyadmin
+$dbname = "usuario";  #database name
+
+#connection string
+$pdo = mysqli_connect($localhost,$dbusername,$dbpassword,$dbname);
+
+if (mysqli_connect_errno()) {
+    echo "Failed to connect to MySQL: " . mysqli_connect_error();
+    exit();
+}
+
+if (isset($_POST["submit"])) {
+    #retrieve file title
+    
+    $title = $_POST["title"];
+    $title = sanitize_filename($title);
+    
+    #file name with a random number so that similar dont get replaced
+    $pname = $title . "-" . $_FILES["file"]["name"];
+    
+    #temporary file name to store file
+    $tname = $_FILES["file"]["tmp_name"];
+   
+    #upload directory path
+    $uploads_dir = 'arquivo';
+    
+    # check file extension
+    $allowed_ext = array('doc', 'docx', 'pdf');
+    $file_ext = strtolower(pathinfo($_FILES["file"]["name"], PATHINFO_EXTENSION));
+    
+    if (!in_array($file_ext, $allowed_ext)) {
+        echo "Only .doc, .docx and .pdf files are allowed";
         exit();
     }
 
-    if (isset($_POST["submit"])) {
-        #retrieve file title
+    #TO move the uploaded file to specific location
+    if (move_uploaded_file($tname, $uploads_dir.'/'.$pname)) {
+        #sql query to insert into database
+        $date = date('Y-m-d');
+        $sql = "INSERT INTO documentos (nome, caminho, propretario, data_upload) VALUES ('$title', 'arquivo/$pname', '{$_SESSION['user']}', '$date')";
         
-        $title = $_POST["title"];
-        $title =   sanitize_filename($title);
-        #file name with a random number so that similar dont get replaced
-        $pname =  $title."-".$_FILES["file"]["name"];
-     
-        #temporary file name to store file
-        $tname = $_FILES["file"]["tmp_name"];
-       
-        #upload directory path
-        $uploads_dir = 'arquivo';
-
-        #TO move the uploaded file to specific location
-        if (move_uploaded_file($tname, $uploads_dir.'/'.$pname)) {
-            #sql query to insert into database
-            $sql = "INSERT into documentos(nome,caminho,propretario) VALUES('$title','arquivo/$pname', '{$_SESSION['user']}')";
-     
-            if(mysqli_query($pdo,$sql)){
-                sanitize_filename($title);
-                echo "File Successfully uploaded";
-            }
-            else{
-                echo "Error inserting data: " . mysqli_error($pdo);
-            }
+        if(mysqli_query($pdo,$sql)){
+            sanitize_filename($title);
+            echo "File Successfully uploaded";
         } else {
-            echo "Escolha um arquivo e use somente letras e numeros";
+            echo "Error inserting data: " . mysqli_error($pdo);
         }
+    } else {
+        echo "Escolha um arquivo e use somente letras e numeros";
     }
-    echo $twig->render('upload.html')
-    ;
+}
+
+echo $twig->render('upload.html');
 ?>
+
