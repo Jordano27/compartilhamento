f75c5d455d3423f4475131591ab131ebeaaa2c66
Jordano Poletto <90645419+Jordano27@users.noreply.github.com>
2023-05-17 00:54:35 -0300

    atualização do compartilhar
    90%  concluido

--- puxarusuarios.php
+++ puxarusuarios.php
@@ -1,3 +1,4 @@
+
 <?php
 session_start();
 
@@ -16,9 +17,14 @@ if (!$pdo) {
 }
 
 // Consultar o banco de dados para obter a lista de usuários
-$sql = $pdo->prepare('SELECT idUsuario, nome from usuarios;');
+$sql = $pdo->prepare('SELECT idUsuario, nome FROM usuarios;');
 $sql->execute();
 $usuarios = $sql->fetchAll(PDO::FETCH_ASSOC);
+
+// Consultar o banco de dados para obter a lista de documentos
+$sql = $pdo->prepare('SELECT idDocumento, nome FROM documentos;');
+$sql->execute();
+$documentos = $sql->fetchAll(PDO::FETCH_ASSOC);
 
 // Obter o ID do usuário logado
 $idUsuario = $_GET['id'];
@@ -28,23 +34,42 @@ if (isset($_POST['usuario']) && isset($_POST['idDocumento'])) {
   // Obter o usuário selecionado
   $idUsuarioDestino = $_POST['usuario'];
 
-  // Obter o arquivo enviado
+  // Obter o documento selecionado
   $idDocumento = $_POST['idDocumento'];
 
-  // Inserir o arquivo na tabela de compartilhamentos
-  $sql = $pdo->prepare('INSERT INTO compartilhamentos (idUsuario, idDocumento) VALUES (?, ?)');
-  $sql->execute([ $idUsuarioDestino, $idDocumento]);
+  // Verificar se o documento existe
+  $documentoExists = false;
+  foreach ($documentos as $documento) {
+    if ($documento['idDocumento'] == $idDocumento) {
+      $documentoExists = true;
+      break;
+    }
+  }
 
-  // Redirecionar para a página de compartilhamentos
-  header('Location: arquivoscompartilhados.php');
-  exit();
+  if ($documentoExists) {
+    // Inserir o documento na tabela de compartilhamentos
+    $sql = $pdo->prepare('INSERT INTO compartilhamentos (idUsuario, idDocumento) VALUES (?, ?)');
+    $sql->execute([$idUsuarioDestino, $idDocumento]);
+
+    // Redirecionar para a página de compartilhamentos
+    header('Location: arquivoscompartilhados.php');
+    exit();
+  } else {
+    echo "Documento inválido. Por favor, selecione um documento válido.";
+  }
 }
 
 // Exibir o formulário
 echo "<form method='POST' enctype='multipart/form-data'>";
-echo "<input type='hidden' name='idDocumento' value='1'>";
+echo "<label>Selecione um documento:</label>";
+echo "<select name='idDocumento'>";
+foreach ($documentos as $documento) {
+  echo "<option value='" . $documento['idDocumento'] . "'>" . $documento['nome'] . "</option>";
+}
+echo "</select>";
+echo "<br><br>";
 echo "<label>Selecione um usuário:</label>";
-echo "<select name='usuario'>";
+echo "<select name='usuario' >";
 foreach ($usuarios as $usuario) {
   if ($usuario['idUsuario'] != $idUsuario) {
     echo "<option value='" . $usuario['idUsuario'] . "'>" . $usuario['nome'] . "</option>";
@@ -57,5 +82,4 @@ echo "</form>";
 
 // Fechar a conexão
 $pdo = null;
-
 ?>
